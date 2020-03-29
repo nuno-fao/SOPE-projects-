4a0c31403a5cd4a2bbc033df3ec0d6649582f24e
Nunation <oliveiranunopt@gmail.com>
2020-03-29 18:54:20 +0100

    nada demais

---
+++ simpledu.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+
+int main(int argc, char **argv)
+{
+  printf("Hello World!\n"); 
+  return 0;
+}
