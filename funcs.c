f6f1c4e5ca40cfa41b99274d2536d1fc08bc5883
Nunation <oliveiranunopt@gmail.com>
2020-03-30 17:49:55 +0100

    invalidArg bugfix

--- funcs.c
+++ funcs.c
@@ -13,8 +13,34 @@ bool invalidArgs(char **argv, int argc ){
 	char *separate2 = "--separate-dirs";
 	char *maxDepth = "--max-depth=";
 
-	if(strncmp(link1,(*argv)[1],2)||strncmp(link2,(*argv)[1],13)){
-		return false;
+	if(!strncmp(link1,*(argv+1),2)&&!strncmp(link2,*(argv+1),13)){
+		return true;
 	}
-	return true;
+
+	for(char **arg=argv+2;*arg!=0;arg++){
+		char *thisarg=*arg;
+		if(strncmp(bytes1,thisarg,2)||strncmp(bytes2,thisarg,7)){
+			//something
+			continue;
+		}
+		else if(strncmp(block1,thisarg,2)||strncmp(block2,thisarg,13)){
+			//something
+			continue;
+		}
+		else if(strncmp(dereference1,thisarg,2)||strncmp(dereference2,thisarg,13)){
+			//something
+			continue;
+		}
+		else if(strncmp(separate1,thisarg,2)||strncmp(dereference2,thisarg,15)){
+			//something
+			continue;
+		}
+		else if(strncmp(maxDepth,thisarg,12)){
+			//something
+			continue;
+		}
+		return true;
+	}
+
+	return false;
 }
--- funcs.h
+++ funcs.h
@@ -1,4 +1,4 @@
 #include <stdbool.h>
 #include <string.h>
 
-bool invalidArgs(char **argv);
+bool invalidArgs(char **argv, int argc);
