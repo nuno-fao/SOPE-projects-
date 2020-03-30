5e5c778679e71a31604677e0129351b3a73ab464
Nunation <oliveiranunopt@gmail.com>
2020-03-30 19:14:57 +0100

    finished arguments function

--- funcs.c
+++ funcs.c
@@ -7,13 +7,13 @@ bool invalidArgs(char **argv, int argc ){
 	char *all2 = "--all\0";
 	char *bytes1 = "-b\0";
 	char *bytes2 = "--bytes\0";
-	char *block1 = "-B";
+	char *block1 = "-B\0";
 	char *block2 = "--block-size=";
 	char *dereference1 = "-L\0";
 	char *dereference2 = "--dereference\0";
 	char *separate1 = "-S\0";
 	char *separate2 = "--separate-dirs\0";
-	char *maxDepth = "--max-depth=";
+	char *max_depth = "--max-depth=";
 
 	if(strncmp(link1,*(argv+1),3)!=0 && strncmp(link2,*(argv+1),13)!=0){
 		return true;
@@ -31,8 +31,15 @@ bool invalidArgs(char **argv, int argc ){
 			printf("check 2\n");
 			continue;
 		}
-		else if(strncmp(block1,thisarg,2)==0||strncmp(block2,thisarg,13)==0){
-			//something
+		else if(strncmp(block1,thisarg,3)==0||strncmp(block2,thisarg,13)==0){
+			if(strncmp(block1,thisarg,3)==0){
+				arg++;
+				blockSize=atoi(*arg);
+			}
+			else{
+				thisarg+=13;
+				blockSize=atoi(thisarg);
+			}
 			continue;
 		}
 		else if(strncmp(dereference1,thisarg,3)==0||strncmp(dereference2,thisarg,14)==0){
@@ -45,8 +52,9 @@ bool invalidArgs(char **argv, int argc ){
 			printf("check 4\n");
 			continue;
 		}
-		else if(strncmp(maxDepth,thisarg,12)==0){
-			//something
+		else if(strncmp(max_depth,thisarg,12)==0){
+			thisarg+=12;
+			maxDepth=atoi(thisarg);
 			continue;
 		}
 		return true;
