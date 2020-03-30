c593566699685318dc56d619ec11c9c01252d238
Nunation <oliveiranunopt@gmail.com>
2020-03-30 18:59:20 +0100

    progress on saving arguments

--- funcs.c
+++ funcs.c
@@ -1,41 +1,51 @@
 #include "funcs.h"
 
 bool invalidArgs(char **argv, int argc ){
-	char *link1 = "-l";
-	char *link2 = "--count-links";
-	char *bytes1 = "-b";
-	char *bytes2 = "--bytes";
+	char *link1 = "-l\0";
+	char *link2 = "--count-links\0";
+	char *all1 = "-a\0";
+	char *all2 = "--all\0";
+	char *bytes1 = "-b\0";
+	char *bytes2 = "--bytes\0";
 	char *block1 = "-B";
 	char *block2 = "--block-size=";
-	char *dereference1 = "-L";
-	char *dereference2 = "--dereference";
-	char *separate1 = "-S";
-	char *separate2 = "--separate-dirs";
+	char *dereference1 = "-L\0";
+	char *dereference2 = "--dereference\0";
+	char *separate1 = "-S\0";
+	char *separate2 = "--separate-dirs\0";
 	char *maxDepth = "--max-depth=";
 
-	if(!strncmp(link1,*(argv+1),2)&&!strncmp(link2,*(argv+1),13)){
+	if(strncmp(link1,*(argv+1),3)!=0 && strncmp(link2,*(argv+1),13)!=0){
 		return true;
 	}
 
-	for(char **arg=argv+2;*arg!=0;arg++){
+	for(char **arg=argv+3;*arg!=0;arg++){
 		char *thisarg=*arg;
-		if(strncmp(bytes1,thisarg,2)||strncmp(bytes2,thisarg,7)){
+		if(strncmp(all1,thisarg,3)==0||strncmp(all2,thisarg,6)==0){
+			all=true;
+			printf("check 1\n");
+			continue;
+		}
+		else if(strncmp(bytes1,thisarg,3)==0||strncmp(bytes2,thisarg,8)==0){
+			bytes=true;
+			printf("check 2\n");
+			continue;
+		}
+		else if(strncmp(block1,thisarg,2)==0||strncmp(block2,thisarg,13)==0){
 			//something
 			continue;
 		}
-		else if(strncmp(block1,thisarg,2)||strncmp(block2,thisarg,13)){
-			//something
+		else if(strncmp(dereference1,thisarg,3)==0||strncmp(dereference2,thisarg,14)==0){
+			dereference=true;
+			printf("check 3\n");
 			continue;
 		}
-		else if(strncmp(dereference1,thisarg,2)||strncmp(dereference2,thisarg,13)){
-			//something
+		else if(strncmp(separate1,thisarg,3)==0||strncmp(dereference2,thisarg,16)==0){
+			separate=true;
+			printf("check 4\n");
 			continue;
 		}
-		else if(strncmp(separate1,thisarg,2)||strncmp(dereference2,thisarg,15)){
-			//something
-			continue;
-		}
-		else if(strncmp(maxDepth,thisarg,12)){
+		else if(strncmp(maxDepth,thisarg,12)==0){
 			//something
 			continue;
 		}
--- funcs.h
+++ funcs.h
@@ -1,4 +1,13 @@
+#pragma once
 #include <stdbool.h>
 #include <string.h>
+#include <stdio.h>
+
+extern bool all;
+extern bool bytes;
+extern bool dereference;
+extern bool separate;
+extern int blockSize;
+extern int maxDepth;
 
 bool invalidArgs(char **argv, int argc);
